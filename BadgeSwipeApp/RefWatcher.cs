0e3a3b2a6a83dd7ff79f4f364f90581d75a652d8
Matt Fennell <matthew.fennell@wallawalla.edu>
2018-06-06 19:38:52 -0700

    Copied SwipeWatcher model into RefWatcher

--- BadgeSwipeApp/RefWatcher.cs
+++ BadgeSwipeApp/RefWatcher.cs
@@ -2,11 +2,61 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using TableDependency.Enums;
+using TableDependency.EventArgs;
+using TableDependency.SqlClient;
 
 namespace BadgeSwipeApp
 {
     class RefWatcher
     {
+        public void DependencyWatch(
+            System.ComponentModel.BackgroundWorker worker,
+            System.ComponentModel.DoWorkEventArgs e)
+        {
+            var connectionString = "Server = 192.168.176.133; " +
+                                    "Database=Badge_Swipe_EntryDB;" +
+                                    "Trusted_Connection=yes";
+            var tableDependency = new SqlTableDependency<RefData>(connectionString, "RefData");
+
+            using (tableDependency)
+            {
+                tableDependency.OnChanged += TableDependency_Changed;
+                tableDependency.OnError += TableDependency_OnError;
+                tableDependency.Start();
+
+                while (!worker.CancellationPending)
+                {
+
+                }
+
+                tableDependency.Stop();
+
+            }
+
+        }
+
+        static void TableDependency_Changed(object sender, RecordChangedEventArgs<RefData> e)
+        {
+
+            if (e.ChangeType == ChangeType.Insert)
+            {
+                var changedEntity = e.Entity;
+                if (GlobalVar.StartRef == 0)
+                {
+                    GlobalVar.StartRef = changedEntity.entry_number;
+                }
+                GlobalVar.RefNum += 1;
+
+            }
+        }
+
+        static void TableDependency_OnError(object sender, ErrorEventArgs e)
+        {
+            Exception ex = e.Error;
+            throw ex;
+        }
     }
 }
